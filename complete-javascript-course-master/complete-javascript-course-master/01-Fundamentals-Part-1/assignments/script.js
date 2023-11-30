e1cd28082e1b0f68f4845cd0673c1799dcfcb11a
Web Development <141753914+bjakeharr@users.noreply.github.com>
2023-11-30 10:35:39 -0700

    updated assignment and code along

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/assignments/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/assignments/script.js
@@ -1,14 +1,34 @@
-let country = "USA";
-let continent = "North America";
+const country = "USA";
+const continent = "North America";
 let population = 331000000;
 console.log(country);
 console.log(continent);
 console.log(population);
 
 //data types
-let isIsland = false;
+const isIsland = false;
 let language;
 console.log(typeof country);
 console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof language);
+
+//Let const var
+language = "English";
+
+population++;
+console.log(population);
+
+let popFin = 33000000;
+const popAvg = 33000000;
+console.log(population > popFin);
+console.log(population > popAvg);
+
+let description =
+	country +
+	" is in " +
+	continent +
+	" and its " +
+	population +
+	" people speak " +
+	language;
--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -25,7 +25,7 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
-*/
+
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -46,3 +46,48 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtman";
+console.log(lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 **3 equals 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
