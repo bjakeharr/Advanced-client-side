5adef70f56197cb08cc1aa1ea97c06aa9aa923ce
Web Development <141753914+bjakeharr@users.noreply.github.com>
2023-11-30 09:38:34 -0700

    updated assignments and code along

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/assignments/script.js
@@ -0,0 +1,14 @@
+let country = "USA";
+let continent = "North America";
+let population = 331000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//data types
+let isIsland = false;
+let language;
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
