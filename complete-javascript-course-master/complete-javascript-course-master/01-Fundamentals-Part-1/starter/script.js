66de0ccf087e454d24cd1658ce27c4a988f27e0e
Web Development <141753914+bjakeharr@users.noreply.github.com>
2023-12-01 09:04:14 -0700

    updated assignments and starter code for type conversion

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -145,7 +145,7 @@ if (birthYear <= 2000) {
 
 console.log(century);
 */
-
+//Type conversion
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
@@ -154,3 +154,13 @@ console.log(Number("Jonas"));
 console.log(typeof Nan);
 
 console.log(String(23), 23);
+
+//type coercion
+console.log("I am " + 23 + " years old.");
+console.log("I am" + " 23 " + "years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
