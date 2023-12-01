a85eee6ebeac7afe625e5b06444289d78b086c34
Web Development <141753914+bjakeharr@users.noreply.github.com>
2023-12-01 12:26:11 -0700

    updated starter code for section 2 with function lecture code along and assignment.

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,42 @@
 "use strict"; //strict mode must be put first
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio";
 // const private = 534;
+
+//Functions
+// function logger() {
+// 	console.log("My name is Jonas.");
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+// 	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+// 	return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//functions assignment
+
+function describeCountry(country, population, capitalCity) {
+	const result = `${country} has a population of ${population} people and its capital city is ${capitalCity}`;
+	return result;
+}
+const country1 = describeCountry("Finland", 6000000, "Helsinki");
+const country2 = describeCountry("Japan", 125000000, "Tokyo");
+const country3 = describeCountry("Ireland", 5033000, "Dublin");
+
+console.log(country1);
+console.log(country2);
+console.log(country3);
