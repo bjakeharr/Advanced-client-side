98747662fa42e5c024a39da4fa469d976fed03fd
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2023-12-02 12:56:35 -0700

    part 2 updated for arrow functions

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -44,33 +44,57 @@
 //functions declarations and expressions
 
 //function declaration
-function calcAge1(birthYear) {
-	return 2037 - birthYear;
-}
+// function calcAge1(birthYear) {
+// 	return 2037 - birthYear;
+// }
 
-const age1 = calcAge1(1991);
-console.log(age1);
+// const age1 = calcAge1(1991);
+// console.log(age1);
 
-//function expression
-const calcAge2 = function (birthYear) {
-	return 2037 - birthYear;
+// //function expression
+// const calcAge2 = function (birthYear) {
+// 	return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+// //function declaration and expression assiignment
+// function percentageOfWorld1(population) {
+// 	return (population / 7900) * 100;
+// }
+// const usaPercentage = percentageOfWorld1(330);
+// const irelandPercentage = percentageOfWorld1(5);
+// const japanPercentage = percentageOfWorld1(125);
+// console.log(usaPercentage, irelandPercentage, japanPercentage);
+
+// const percentageOfWorld2 = function (population) {
+// 	return (population / 7900) * 100;
+// };
+// const usaPercentage2 = percentageOfWorld2(330);
+// const irelandPercentage2 = percentageOfWorld2(5);
+// const japanPercentage2 = percentageOfWorld2(125);
+// console.log(usaPercentage2, irelandPercentage2, japanPercentage2);
+
+//arrow functions
+
+//arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+	const age = 2037 - birthYear;
+	const retirement = 65 - age;
+	// return retirement;
+	return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
 
-//function declaration and expression assiignment
-function percentageOfWorld1(population) {
-	return (population / 7900) * 100;
-}
-const usaPercentage = percentageOfWorld1(330);
-const irelandPercentage = percentageOfWorld1(5);
-const japanPercentage = percentageOfWorld1(125);
-console.log(usaPercentage, irelandPercentage, japanPercentage);
+//arrow functions assignment
 
-const percentageOfWorld2 = function (population) {
-	return (population / 7900) * 100;
-};
-const usaPercentage2 = percentageOfWorld2(330);
-const irelandPercentage2 = percentageOfWorld2(5);
-const japanPercentage2 = percentageOfWorld2(125);
-console.log(usaPercentage2, irelandPercentage2, japanPercentage2);
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+const usaPercentage3 = percentageOfWorld3(330);
+const irelandPercentage3 = percentageOfWorld3(5);
+const japanPercentage3 = percentageOfWorld3(125);
+console.log(usaPercentage3, irelandPercentage3, japanPercentage3);
