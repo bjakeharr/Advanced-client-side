de9504e8247dbb3ba1ade6f24c12c5f27871956a
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2023-12-04 10:59:58 -0700

    update script.js
    Updated to reflect array methods lecture and assignment

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -150,57 +150,104 @@ function percentageOfWorld1(population) {
 // console.log(yearsUntilRetirement(1970, "Mike"));
 
 //Intro to arrays
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
 
-const friends = ["Michael", "Steven", "Peter"];
-console.log(friends);
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
 
-const yy = new Array(1991, 1984, 2008, 2020);
+// const yy = new Array(1991, 1984, 2008, 2020);
 
-console.log(friends[0]);
-console.log(friends[2]);
+// console.log(friends[0]);
+// console.log(friends[2]);
 
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = "Jay";
-console.log(friends);
+// friends[2] = "Jay";
+// console.log(friends);
 
-const firstName = "Jonas";
-const jonas = [firstName, "Schmedtman", 2037 - 1991, "teacher", friends];
-console.log(jonas);
+// const firstName = "Jonas";
+// const jonas = [firstName, "Schmedtman", 2037 - 1991, "teacher", friends];
+// console.log(jonas);
 
-const calcAge = function (birthYear) {
-	return 2037 - birthYear;
-};
+// const calcAge = function (birthYear) {
+// 	return 2037 - birthYear;
+// };
 
-const years = [1990, 1967, 2002, 2010, 2018];
+// const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
 
-const ages = [
-	calcAge(years[0]),
-	calcAge(years[1]),
-	calcAge(years[years.length - 1]),
-];
-console.log(ages);
+// const ages = [
+// 	calcAge(years[0]),
+// 	calcAge(years[1]),
+// 	calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
 
-//arrays assignment
-const populations = [330, 5, 125, 1170];
-if (populations.length === 4) {
-	console.log(`populations array contains ${populations.length} variables`);
+// //arrays assignment
+// const populations = [330, 5, 125, 1170];
+// if (populations.length === 4) {
+// 	console.log(`populations array contains ${populations.length} variables`);
+// }
+
+// const percentages = [
+// 	percentageOfWorld1(populations[0]),
+// 	percentageOfWorld1(populations[1]),
+// 	percentageOfWorld1(populations[2]),
+// 	percentageOfWorld1(populations[3]),
+// ];
+
+// console.log(percentages);
+
+// //Array methods
+// const friends = ["Michael", "Steven", "Peter"];
+
+// //add elements
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift("John");
+// console.log(friends);
+
+// //remove elements
+
+// friends.pop(); //removes last in array.
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift(); //remove first
+// console.log(friends);
+
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.indexOf("Bob"));
+
+// friends.push(23);
+// console.log(friends.includes("Bob"));
+// console.log(friends.includes("STeven"));
+// console.log(friends.includes("23"));
+// console.log(friends.includes(23));
+
+// if (friends.includes("Peter")) {
+// 	console.log(`You have a friend called Peter`);
+// }
+
+//array methods assignment
+const neighbors = new Array("Mexico", "Canada");
+neighbors.push("Utopia");
+console.log(neighbors.length, neighbors);
+neighbors.pop();
+console.log(neighbors.length, neighbors);
+
+if (!neighbors.includes("Germany")) {
+	console.log(`Probably not a central European country`);
 }
-
-const percentages = [
-	percentageOfWorld1(populations[0]),
-	percentageOfWorld1(populations[1]),
-	percentageOfWorld1(populations[2]),
-	percentageOfWorld1(populations[3]),
-];
-
-console.log(percentages);
+neighbors[1] = "Canadia";
+console.log(neighbors);
