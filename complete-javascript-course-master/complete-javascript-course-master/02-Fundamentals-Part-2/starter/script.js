66834ff4f7987b9493225a261a1237e57a93e5be
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2023-12-04 12:36:25 -0700

    update script.js
    updated to reflect object methods. still need to do assignment

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -319,3 +319,42 @@ const myCountry = {
 // console.log(
 // 	`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbors.length} neighboring countries, and a capital called ${myCountry.capital}.`,
 // );
+
+//object methods
+
+const jonas = {
+	firstName: "Jonas",
+	lastName: "Schmedtman",
+	birthYear: 1991,
+	job: "teacher",
+	friends: ["Michael", "Peter", "Steven"],
+	hasDriversLicense: true,
+
+	// calcAge: function (birthYear) {
+	// 	return 2037 - birthYear;
+	// },
+
+	// calcAge: function () {
+	// 	return 2037 - this.birthYear;
+	// },
+	calcAge: function () {
+		this.age = 2037 - this.birthYear;
+		return this.age;
+	},
+
+	getSummary: function () {
+		return `${this.firstName} is a ${this.age}-year old ${this.job}, and ${
+			this.hasDriversLicense
+				? ` he has a driver's license.`
+				: `he does not have a driver's license`
+		}`;
+	},
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
