0917d2093f1fa7908146e1a7b8eeb2778e66ca08
Web Development <141753914+bjakeharr@users.noreply.github.com>
2023-12-07 09:57:25 -0700

    update script.js
    added functionality to amend styles on correct guess

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -22,9 +22,16 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
+    //when no input
     document.querySelector(`.message`).textContent = 'No Number!';
-  } else if (guess === secretNumber) {
+  }
+
+  //when guess is correct
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = `Correct Number!`;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
     //responses for incorrect guesses.
   } else if (guess > secretNumber) {
     if (score > 1) {
