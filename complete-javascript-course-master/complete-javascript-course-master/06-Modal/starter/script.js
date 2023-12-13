90d2c346ae168821f8868e4946ee432191d964b9
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2023-12-12 17:52:36 -0700

    Update script.js
    fixed mistype on document.queryselector

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = documetn.querySelector(`.close-modal`);
+const btnCloseModal = document.querySelector(`.close-modal`);
 const btnsOpenModal= document.querySelectorAll(`.show-modal`);
 const closeModal = function () {
     modal.classList.add(`hidden`);
