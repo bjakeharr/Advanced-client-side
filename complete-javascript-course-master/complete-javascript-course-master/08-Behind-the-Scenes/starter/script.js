33966a5f102f11da748bd1e41e065d00e6d8db8a
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2023-12-17 14:05:23 -0700

    Update script.js
    added notes to advanced JS overview in the script

--- complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+//notes will be on the code
+
+//high level - the developer does not have to manage computer resources. limits optimization but less stressful
+//garbage collected - cleans up the memory not in use automatically
+//interpreted or just in time compiled- interprets human readable codde to machine code within the javascript engine itself.
+//multi-paradigm - supports multiple programming approaches procedural vs object oriented
+//prototyped based object oriented- almost everything in js is an object 
+//first class functions- functions are treated as variables which means they can be passed within other functions.
+//dynamic - data types are automatically assigned instead of explicitly defined for a variable
+//single-threaded - javascript can only run one thing at a time
+//non-blocking behavior- uses an event loop to run long running tasks in the background, suspends them when needed then moves them back to the front of the line when needed.
