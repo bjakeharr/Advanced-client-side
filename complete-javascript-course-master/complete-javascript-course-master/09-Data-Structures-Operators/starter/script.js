6977babfd340474a30d7f564e7696d4f81410b7c
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-03 12:31:57 -0700

    Update script.js
    coding challenge in progress

--- complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -279,4 +279,84 @@ const rest2 = {
 
 // const guestCorrect = restaurant.numGuests ?? 10;
 
-/////////////Logical assignment operators
+//coding challenge one
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1.
+let [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+//2.
+let [gk, ...fieldplayers] = players1;
+console.log(gk);
+console.log(fieldplayers);
+
+//3.
+const [...allPlayers] = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.
+let [...playersFinal] = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(playersFinal);
+
+//5.
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+//6.
+
+const printGoals = function (...playerNames) {
+  for (let i = 0; i < playerNames.length; i++) {
+    console.log(playerNames[i]);
+  }
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(game.scored);
+
+//7.
+let team1Win = game.odds.team2 > game.odds.team1;
+let team2Win = game.odds.team1 > game.odds.team2;
+console.log(team1Win && team2Win);
