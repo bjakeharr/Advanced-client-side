e0003279c559ba45f62e5106b8e80812739d997d
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-11 09:33:20 -0700

    ch 10 lecture complete
    completed value vs reference

--- complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,30 +1,57 @@
 'use strict';
 //-----------Default parameters-------
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  //ES5 method
-  //   numPassengers = numPassengers || 1;
-  //   price = price ||199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//ES5 method
+//   numPassengers = numPassengers || 1;
+//   price = price ||199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
 
 //setting a parameter to undefined allows the default value to still be used.
-createBooking('LH123', undefined, 1000);
+// createBooking('LH123', undefined, 1000);
+
+//---------------How passing arguments woks: value versus reference-------------
+
+// const flight = 'Lh234';
+// const jonas = {
+//   name: 'Jonas Schmedttman',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr.' + passenger.name;
+//   passenger.passport === 24739479284
+// ? alert('checked in')
+//     : alert('wrong passport');
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
