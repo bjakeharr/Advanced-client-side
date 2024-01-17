de34405038c8a42771204f17b6319bea2f88f4cf
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2024-01-17 10:30:28 -0700

    update ch11
    completed lecture for ch 11

--- complete-javascript-course-master/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -228,7 +228,7 @@ createUsernames(accounts);
 
 //------- the map method
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const eurToUsd = 1.1;
 
 // const movementsUSD = movements.map(function (mov) {
@@ -252,3 +252,22 @@ createUsernames(accounts);
 //       mov
 //     )}`
 // );
+
+//---------The filter method
+//callback function array.filter(function (value,i,array){boolean ex: value > 0})
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(deposits);
+// const depositsFor = [];
+// for (const mov of movements) {
+//   if (mov > 0) {
+//     depositsFor.push(mov);
+//   }
+// }
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+// console.log(withdrawals);
