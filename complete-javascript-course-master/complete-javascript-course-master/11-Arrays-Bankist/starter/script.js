8fdcce9b8f029acb5a3668729316696bebc7750b
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-19 12:06:31 -0700

    update ch 11
    completed array methods practice

--- complete-javascript-course-master/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -524,3 +524,49 @@ btnClose.addEventListener('click', function (e) {
 //   );
 //   console.log(movementsUI);
 // });
+
+//--------Array methods practice
+//1
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(bankDepositSum);
+
+//2
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .filter(mov => mov >= 1000).length;
+  .reduce((acc, el, i) => (el >= 1000 ? acc + 1 : acc), 0);
+console.log(numDeposits1000);
+
+//3
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      //   cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      //   return sums;
+      // }
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals);
+
+//4
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'and', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase(`this is a nice title`));
+console.log(convertTitleCase(`this is a LONG title but not too long`));
+console.log(convertTitleCase(`and here is another title with an EXAMPLE`));
