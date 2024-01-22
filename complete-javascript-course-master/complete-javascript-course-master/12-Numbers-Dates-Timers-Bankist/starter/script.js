5182a4d1b40f146e8c1a7284f7b51f2c2ff2e466
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-22 10:42:31 -0700

    update ch12
    update for converting and checking numbers

--- complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,36 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+//-------Converting and checking numbers
+//in JS numbers are represented internally as floating points ie decimals
+
+// console.log(23 === 23.0);
+
+//base ten is numbers 0-9
+//base 2 is 0 and 1
+
+// console.log(Number('23'));
+//you can add a + operator to the front of a string to convert it to a number.
+// console.log(+'23');
+
+//Parsing
+
+//since 'Number' is an object, it has methods that can be called
+//Number.parseInt() will parse a string to try and determine the number value contained within. The string NEEDS to start with a number to work.
+// Number.parseInt('string starting w number', number model ie base 10)
+// console.log(Number.parseInt('30px', 10));
+
+//parseFloat is for decimal values.
+// console.log(Number.parseFloat('2.5rem'));
+
+//check if value is Nan
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
+
+//isFinite is better suited to check if a number is a number instead of a string
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
