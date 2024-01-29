5b4751c8048f411ee9cbe53fe14bcfec58116c6e
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2024-01-29 14:48:26 -0700

    update ch 13
    completed select, create and delete lecture

--- complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//------selecting , creating, and deleting elements
+//selecting an element
+
+//selecting via document
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+//selecting via class name
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+//selecting by Id
+document.getElementById('section--1');
+
+//selecting by tag or class name (old mehtods)
+const allButtons = document.getElementsByTagName('button');
+
+document.getElementsByClassName('btn');
+
+//creating and inserting elements
+//.insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class = "btn btn--close--cookie">Got it</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+
+//delete elements
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
