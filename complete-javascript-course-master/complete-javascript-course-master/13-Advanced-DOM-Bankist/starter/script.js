73268f26980943b7886aac7f5a2d80800dc35151
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-30 10:34:42 -0700

    update ch 13
    complete dom traversal lecture

--- complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -161,3 +161,38 @@ document
 // document.querySelector('.nav').addEventListener('click', function (e) {
 //   console.log('LINK');
 // });
+
+///---DOM traversal
+// const h1 = document.querySelector('h1');
+
+//going downwards: child
+//you can select child elements by using the parent element as a subselector when using query
+// console.log(h1.querySelectorAll('.highlight'));
+//this gives nodes
+// console.log(h1.childNodes);
+//this gives live HTML direct child elements
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+//going upwards with parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//selecting siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) {
+//     el.style.transform = 'scale(0.5)';
+//   }
+// });
