9419a264ec8bca98ad5dcac23fa7b84dbfa097e2
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-31 11:29:09 -0700

    update ch14
    completed coding challenge 1

--- complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
@@ -12,11 +12,11 @@ const Person = function (firstName, birthYear) {
 };
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// console.log(jonas);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+// console.log(matilda, jack);
 
 //prototypes
 //All objects made by a constructor function can inherit methods and variables assigned to the original by way of prototypal inheritance.
@@ -24,5 +24,45 @@ Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-jonas.calcAge();
-console.log(jonas);
+// jonas.calcAge();
+// console.log(jonas);
+
+// console.log(jonas.__proto__);
+//object.prototype is typically top of chain
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+const arr = [3, 6, 5, 5, 7, 9, 3, 6, 7, 8, 9];
+
+console.log(arr.__proto__.__proto__);
+
+//you can attach a method to a prototype of a constructor. (dont do this though due to updates)
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+// console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+//==============Coding Challenge 1 =================
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// const mustang = new Car('Mustang', 100);
+// const durango = new Car('Durango', 80);
+
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(`${this.make} has accelerated to ${this.speed}Km/h!`);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed = this.speed - 5;
+//   console.log(`${this.make} has decelerated to ${this.speed}Km/h!`);
+// };
+
+// mustang.accelerate();
+// durango.accelerate();
+// mustang.brake();
