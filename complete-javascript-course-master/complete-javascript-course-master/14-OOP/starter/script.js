e947662e35502468f422db39f85076f0b414f823
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-31 10:30:06 -0700

    update ch14
    completed lecture for prototype inheritance

--- complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,7 +1,14 @@
 'use strict';
+//===constructor function
+//typical naming structure of constructors includes a beginning capital letter.
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  //dont create methods inside of constructor functions. THis bogs down resulting objects
+  //   this.calcAge = function(){
+  //     console.log(2037-birthYear);
+  //   }
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -10,3 +17,12 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
+
+//prototypes
+//All objects made by a constructor function can inherit methods and variables assigned to the original by way of prototypal inheritance.
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+console.log(jonas);
