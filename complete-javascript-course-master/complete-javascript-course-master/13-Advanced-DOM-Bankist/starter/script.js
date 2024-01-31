9fd68caafd222fc532ecb7856ce6748b66279c14
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2024-01-30 20:07:43 -0700

    update ch13
    integrated slider part 1

--- complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -217,6 +217,48 @@ imgTargets.forEach(function (img) {
   imgObserver.observe(img);
 });
 
+//===============The SLIDER
+
+//
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+// slider.style.overflow = 'visible';
+let curSlide = 0;
+const maxSlides = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlides - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlides - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
 //------selecting , creating, and deleting elements
 //selecting an element
 
