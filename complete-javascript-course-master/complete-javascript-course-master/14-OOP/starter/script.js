33236b4340328f0dd35d601925e903ed74bfe066
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-01-31 10:03:34 -0700

    update ch 14
    completed lecture on constructors

--- complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
