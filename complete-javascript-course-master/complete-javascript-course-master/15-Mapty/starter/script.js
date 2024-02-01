597110a78f4351462db4fdcc77222a1eab83e249
bjakeharr <141753914+bjakeharr@users.noreply.github.com>
2024-02-01 15:57:33 -0700

    update ch 15
    form submission is broken

--- complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 //=====Geolocation API===========
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -29,12 +31,40 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      //=============Bind event handler to click and display pin
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert(`Could not obtain current position`);
     }
   );
+
+//=========form event listener
+form.addEventListener('submit', function (e) {
+  inputCadence.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  console.log(mapEvent);
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
