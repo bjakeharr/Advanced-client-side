cc12f90dd6d3021f1ffff357a6cd1a2be3e92032
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-02-06 11:13:11 -0700

    update ch 16
    updated for coding challenge 1

--- complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -114,22 +114,74 @@ const request = fetch(
 console.log(request);
 
 //a promise is an object that holds a spot for a value yet to be filled
-
+const getJSON = function (url, errorMsg = 'SOmething went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
 const getCountryData = function (country) {
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then(response => response.json())
+  getJSON(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    `Something went wrong`
+  )
     .then(data => {
       renderCountry(data[0]);
       const neighbor = data[0].borders[0];
+      console.log(neighbor);
 
-      if (!neighbor) return;
+      if (!neighbor) throw new Error('No neighbor found');
 
       //neighbor
-      return fetch(
-        `https://countries-api-836d.onrender.com/countries/alpha/${neighbor}`
+      return getJSON(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbor}`,
+        `Something went wrong`
       );
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err}`); //catches can be used manually at the end of a chain to compile errors;
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
-getCountryData('usa');
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+
+//coding "challenge" 1
+const whereAmI = function (lat, lng) {
+  //grab data from the API
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (!res.ok) throw new Error('PProblem with Geocode API');
+      //decode the data
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}.`);
+      //pass data from previous query into new request
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/name/${data.country}`
+      );
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with fetching country data`);
+      //decode data from new request
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      //pass data to render function
+      renderCountry(data[0]);
+    })
+    .catch(err => console.error(`${err.message}`));
+};
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
