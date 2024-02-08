a9e0fde953b5e3b031590377f6767f9fad647e78
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-02-08 10:37:35 -0700

    complete ch 17
    had to transpose code from final folder to starter to make things function properly

--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,55 +1,108 @@
-//importing module
+///////////////////////////////////////
+// Exporting and Importing in ES6 Modules
 
-//importing by specific name
+// Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-console.log(`importing module`);
 // addToCart('bread', 5);
 // console.log(price, tq);
-//importing with all
-import shoppingCart, * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
 
-//default imports can be renamed when called upon
-import add from './shoppingCart.js';
-add('muffin', 7);
-add('bread', 3);
+console.log('Importing module');
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
-console.log(ShoppingCart.cart);
+console.log(cart);
+/*
 
-//Top-level await can be used outside of an async function in modules only (ES2022) also blocks execution of code unlike its behavior in an async function
 
-// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 // const data = await res.json();
 // console.log(data);
+// console.log('Something');
 
-//the moduule pattern
-//requires ife
-// const shoppingCart2 = (function () {
-//   const cart = [];
-//   const shippingCost = 10;
-//   const totalPrice = 237;
-//   const totalQuantity = 23;
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
 
-//   const addToCart = function (product, quantity) {
-//     cart.push((product, quantity));
-//     console.log(`${quantity} ${product} added to cart`);
-//   };
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
 
-//   const orderStock = function (product, quantity) {
-//     console.log(`${quantity} ${product} ordered from supplier`);
-//   };
-//   return {
-//     addToCart,
-//     cart,
-//     totalPrice,
-//     totalQuantity,
-//   };
-// })();
+const lastPost = getLastPost();
+console.log(lastPost);
 
-// shoppingCart2.addToCart('apple', 4);
-// shoppingCart2.addToCart('pizza', 2);
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+
+///////////////////////////////////////
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+
+
+///////////////////////////////////////
+// CommonJS Modules
+// Export
+export.addTocart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
+*/
+
+///////////////////////////////////////
+// Introduction to NPM
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -58,9 +111,35 @@ const state = {
   ],
   user: { loggedIn: true },
 };
-
 const stateClone = Object.assign({}, state);
 const stateDeepClone = cloneDeep(state);
-console.log(stateDeepClone);
+
 state.user.loggedIn = false;
 console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,11 +1,16 @@
-//exporting module
-console.log(`exporting module`);
+// Exporting module
+console.log('Exporting module');
+
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
 
 const shippingCost = 10;
 export const cart = [];
 
 export const addToCart = function (product, quantity) {
-  cart.push((product, quantity));
+  cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 };
 
@@ -14,8 +19,7 @@ const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as tq };
 
-//default exports are used for single values and do not have a name when it is a function;
 export default function (product, quantity) {
-  cart.push([product, quantity]);
+  cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 }
