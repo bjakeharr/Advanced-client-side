7273b3c74d1885f1e559e8b8c82cc7afa98faf43
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-02-08 09:28:24 -0700

    update ch 17
    learned about installing with npm

--- complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -49,3 +49,18 @@ console.log(ShoppingCart.cart);
 
 // shoppingCart2.addToCart('apple', 4);
 // shoppingCart2.addToCart('pizza', 2);
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone);
+state.user.loggedIn = false;
+console.log(stateClone);
